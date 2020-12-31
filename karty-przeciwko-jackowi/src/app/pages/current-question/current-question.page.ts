e58be8c92ae361d47f3cece724b2ae408a9e3994
Damian Stachowicz <d.m.stachowicz@gmail.com>
2020-12-31 18:57:03 +0100

    add error dialog

--- karty-przeciwko-jackowi/src/app/pages/current-question/current-question.page.ts
+++ karty-przeciwko-jackowi/src/app/pages/current-question/current-question.page.ts
@@ -1,11 +1,12 @@
+import { AlertController } from '@ionic/angular';
 import { Card, CardType } from '../../interfaces/card.interface';
 import { Component, OnDestroy } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { GameStateService } from 'src/app/services/game-state/game-state.service';
 import { I18nService } from 'src/app/services/i18n/i18n.service';
-import { Player } from 'src/app/interfaces/player.interface';
 import { interval, Subscription } from 'rxjs';
+import { Player } from 'src/app/interfaces/player.interface';
 import { take } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-current-question',
@@ -23,21 +24,42 @@ export class CurrentQuestionPage implements OnDestroy {
 
   constructor(
     public i18n: I18nService,
+    private alertController: AlertController,
     private gameStateService: GameStateService
   ) {
+    this.startStateUpdating();
+  }
+
+  ngOnDestroy() {
+    this.interval$.unsubscribe();
+  }
+
+  private startStateUpdating() {
     this.interval$ = interval(environment.gameUpdateInterval).subscribe(
-      () => this.gameStateService.getState(0).pipe(take(1)).subscribe(
+      () => this.gameStateService.getState(12).pipe(take(1)).subscribe(
         state => {
           this.questionCard = state.currentBlack;
           this.players = state.players;
           this.answerCards = state.hand.map(card => ({ ...card, type: CardType.ANSWER }));
           this.tsarId = state.czarId
-        }
+        },
+        error => this.showErrorDialog(error)
       )
     );
   }
 
-  ngOnDestroy() {
+  private async showErrorDialog(error) {
     this.interval$.unsubscribe();
+
+    const alert = await this.alertController.create({
+      header: this.i18n.get('error'),
+      message: error.message || this.i18n.get('defaultError'),
+      buttons: [{
+        text: 'OK',
+        handler: () => this.startStateUpdating()
+      }]
+    });
+
+    await alert.present();
   }
 }
