db458879350f3d9a7846200c1f6336b5751ddcf1
Damian Stachowicz <d.m.stachowicz@gmail.com>
2021-01-17 19:47:08 +0100

    Move displayAlert method to a service

--- karty-przeciwko-jackowi/src/app/components/card-select/card-select.page.ts
+++ karty-przeciwko-jackowi/src/app/components/card-select/card-select.page.ts
@@ -1,4 +1,3 @@
-import { AlertController, AnimationController } from '@ionic/angular';
 import { Card } from '../../interfaces/card.interface';
 import {
   Component,
@@ -9,6 +8,7 @@ import {
   } from '@angular/core';
 import { GameStateService, Move } from 'src/app/services/game-state/game-state.service';
 import { I18nService } from 'src/app/services/i18n/i18n.service';
+import { AlertService } from 'src/app/services/alert/alert.service';
 
 @Component({
   selector: 'app-card-select',
@@ -24,8 +24,7 @@ export class CardSelectComponent implements OnInit {
 
   constructor(
     public i18n: I18nService,
-    private alertController: AlertController,
-    private animationCtrl: AnimationController,
+    private alertService: AlertService,
     private gameStateService: GameStateService
   ) {}
 
@@ -43,7 +42,7 @@ export class CardSelectComponent implements OnInit {
 
     this.gameStateService.makeAMove(1, Move.PICK, [card.id]).subscribe(
       () => { },
-      error => this.displayAlert(
+      error => this.alertService.displayAlert(
         this.i18n.get('error'),
         error.message || this.i18n.get('defaultError'),
         () => { }
@@ -57,23 +56,5 @@ export class CardSelectComponent implements OnInit {
     if (index > -1) {
       this.cards.splice(index, 1);
     }
-  }
-
-  private async displayAlert(header: string, text: string, handler: () => void) {
-    const alert = await this.alertController.create({
-      header,
-      message: text,
-      buttons: [{ text: 'OK', handler }],
-      enterAnimation: (baseEl: any, opts?: any) => this.animationCtrl
-        .create()
-        .addElement(baseEl.querySelector('.alert-wrapper'))
-        .duration(250)
-        .keyframes([
-          { offset: 0, opacity: '0' },
-          { offset: 1, opacity: '1' }
-      ])
-    });
-
-    await alert.present();
   }
 }
---
+++ karty-przeciwko-jackowi/src/app/services/alert/alert.service.ts
@@ -0,0 +1,31 @@
+import { AlertController, AnimationController } from '@ionic/angular';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor(
+    private alertController: AlertController,
+    private animationCtrl: AnimationController,
+  ) { }
+
+  async displayAlert(header: string, text: string, handler: () => void) {
+    const alert = await this.alertController.create({
+      header,
+      message: text,
+      buttons: [{ text: 'OK', handler }],
+      enterAnimation: (baseEl: any, opts?: any) => this.animationCtrl
+        .create()
+        .addElement(baseEl.querySelector('.alert-wrapper'))
+        .duration(250)
+        .keyframes([
+          { offset: 0, opacity: '0' },
+          { offset: 1, opacity: '1' }
+      ])
+    });
+
+    await alert.present();
+  }
+}
